bdc7652b03f9f8f513912bf239dd55fc692f1664
RoguedBear <39442192+RoguedBear@users.noreply.github.com>
2022-01-15 00:14:48 +0530

    work on settings panel
    default values need to displayed/fixed

--- src/AutoIdleOnAFK/index.js
+++ src/AutoIdleOnAFK/index.js
@@ -1,6 +1,14 @@
 module.exports = (Plugin, Library) => {
     const { Patcher } = Library;
     return class AutoIdleOnAFK extends Plugin {
+        constructor() {
+            super();
+
+            // FIXME: display default values in the slider/radiogroup
+            this.getSettingsPanel = () => {
+                return this.buildSettingsPanel().getElement();
+            };
+        }
         onStart() {
             console.log(this);
         }
