f8dd5e98d30decd565a787acd0dff6249af7bb66
bobbyl140 <61990607+bobbyl140@users.noreply.github.com>
2022-02-01 07:36:52 -0500

    Add setting for always reverting to online

--- src/AutoIdleOnAFK/index.js
+++ src/AutoIdleOnAFK/index.js
@@ -1,57 +1,59 @@
-/*
-MIT License
-
-Copyright (c) 2022 RoguedBear
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
 /**
- * @typedef {import("../../BDPluginLibrary/src/ui/ui").ContextMenu} ContextMenu
- * @typedef {import("../../BDPluginLibrary/src/ui/ui").DiscordContextMenu} DiscordContextMenu
- * @typedef {import("../../BDPluginLibrary/src/ui/ui").Modals} Modals
- * @typedef {import("../../BDPluginLibrary/src/ui/ui").Popouts} Popouts
- * @typedef {import("../../BDPluginLibrary/src/ui/ui").ErrorBoundary} ErrorBoundary
- * @typedef {import("../../BDPluginLibrary/src/ui/ui").Tooltip} Tooltip
- * @typedef {import("../../BDPluginLibrary/src/ui/ui").Toasts} Toasts
- * @typedef {import("../../BDPluginLibrary/src/ui/settings/index")} Settings
- * @typedef {import("../../BDPluginLibrary/src/modules/modules")} Modules
- * @typedef {import("../../BDPluginLibrary/src/structs/plugin.js")} Plugin
+ * @name AutoIdleOnAFK
+ * @authorLink https://github.com/RoguedBear
+ * @version 0.2.0
+ * @website https://github.com/RoguedBear/BetterDiscordPlugin-AutoIdleOnAFK
+ * @source https://github.com/RoguedBear/BetterDiscordPlugin-AutoIdleOnAFK/releases/latest/download/AutoIdleOnAFK.plugin.js
  */
-/**
- * @typedef {Modules & {
- * DiscordContextMenu: DiscordContextMenu
- * DiscordContextMenu: DCM
- * ContextMenu: ContextMenu
- * Tooltip: Tooltip
- * Toasts: Toasts
- * Settings: Settings
- * Popouts: Popouts
- * Modals: Modals
- * }} Library
- */
-/**
- * Creates the plugin class
- * @param {typeof Plugin} Plugin
- * @param {Library} Library
- * @returns {typeof globalThis.Plugin}
- */
-module.exports = (Plugin, Library) => {
+/*@cc_on
+@if (@_jscript)
+    
+    // Offer to self-install for clueless users that try to run this directly.
+    var shell = WScript.CreateObject("WScript.Shell");
+    var fs = new ActiveXObject("Scripting.FileSystemObject");
+    var pathPlugins = shell.ExpandEnvironmentStrings("%APPDATA%\\BetterDiscord\\plugins");
+    var pathSelf = WScript.ScriptFullName;
+    // Put the user at ease by addressing them in the first person
+    shell.Popup("It looks like you've mistakenly tried to run me directly. \n(Don't do that!)", 0, "I'm a plugin for BetterDiscord", 0x30);
+    if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
+        shell.Popup("I'm in the correct folder already.", 0, "I'm already installed", 0x40);
+    } else if (!fs.FolderExists(pathPlugins)) {
+        shell.Popup("I can't find the BetterDiscord plugins folder.\nAre you sure it's even installed?", 0, "Can't install myself", 0x10);
+    } else if (shell.Popup("Should I copy myself to BetterDiscord's plugins folder for you?", 0, "Do you need some help?", 0x34) === 6) {
+        fs.CopyFile(pathSelf, fs.BuildPath(pathPlugins, fs.GetFileName(pathSelf)), true);
+        // Show the user where to put plugins in the future
+        shell.Exec("explorer " + pathPlugins);
+        shell.Popup("I'm installed!", 0, "Successfully installed", 0x40);
+    }
+    WScript.Quit();
+
+@else@*/
+
+module.exports = (() => {
+    const config = {"info":{"name":"AutoIdleOnAFK","authors":[{"name":"RoguedBear","github_username":"RoguedBear"}],"authorLink":"https://github.com/RoguedBear","version":"0.2.0","description":"Automatically updates your discord status to 'idle' when you haven't opened your discord client for more than 5 minutes.\nPlugin only works when your status is 'online' and you are not in a voice channel. \n\nFor Bugs or Feature Requests open an issue on my Github","github":"https://github.com/RoguedBear/BetterDiscordPlugin-AutoIdleOnAFK","github_raw":"https://github.com/RoguedBear/BetterDiscordPlugin-AutoIdleOnAFK/releases/latest/download/AutoIdleOnAFK.plugin.js"},"changelog":[{"type":"added","title":"What's New?","items":["added `Do Not Disturb` as an AFK status option"]},{"type":"fixed","title":"Fixed","items":["fixed an undocumented bug: Status is changed back to `online` from AFK when you join a VC (and then unfocus discord)"]},{"type":"improved","title":"Improved/optimised","items":["avoid repeated webpack searches by searching & storing the modules on startup"]},{"title":"~~~~~~~~~~~~~~~","type":"progress","items":["If you find any bugs or have some feature requests, feel free to open an issue on this plugin's Github","https://github.com/RoguedBear/BetterDiscordPlugin-AutoIdleOnAFK/issues","(PS: thank you codeSpicer & Windy for beta testing the plugin out 👀)"]}],"main":"index.js","DEBUG":false,"DEBUG_ActuallyChangeStatus":false,"defaultConfig":[{"type":"radio","name":"Change Status To:","note":"the status selected will be switched to when AFK. default: Idle","id":"afkStatus","value":"idle","options":[{"name":"Idle","value":"idle"},{"name":"Invisible","value":"invisible"},{"name":"Do Not Disturb","value":"dnd"}]},{"type":"switch","name":"Always Revert To Online","note":"Come back online even if this plugin didn\'t change your status. Useful for multiple devices.","id":"alwaysOnline","value":false,"defaultValue":false},{"type":"slider","name":"AFK Timeout (minutes)","note":"minutes to wait before changing your status to Idle/Invisible","id":"afkTimeout","value":5,"defaultValue":5,"min":5,"max":30,"units":"min","markers":[5,10,15,20,25,30]},{"type":"slider","name":"Back To Online Delay (Grace/Cooldown Period)","note":"seconds to wait before changing your status back to Online. if you close or unfocus discord window, status will not be changed","id":"backToOnlineDelay","defaultValue":10,"value":10,"min":5,"max":120,"units":"s","markers":[5,10,30,60,90,120]}]};
+
+    return !global.ZeresPluginLibrary ? class {
+        constructor() {this._config = config;}
+        getName() {return config.info.name;}
+        getAuthor() {return config.info.authors.map(a => a.name).join(", ");}
+        getDescription() {return config.info.description;}
+        getVersion() {return config.info.version;}
+        load() {
+            BdApi.showConfirmationModal("Library Missing", `The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`, {
+                confirmText: "Download Now",
+                cancelText: "Cancel",
+                onConfirm: () => {
+                    require("request").get("https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js", async (error, response, body) => {
+                        if (error) return require("electron").shell.openExternal("https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js");
+                        await new Promise(r => require("fs").writeFile(require("path").join(BdApi.Plugins.folder, "0PluginLibrary.plugin.js"), body, r));
+                    });
+                }
+            });
+        }
+        start() {}
+        stop() {}
+    } : (([Plugin, Api]) => {
+        const plugin = (Plugin, Library) => {
     const { Logger, DiscordModules } = Library;
     const {
         SelectedChannelStore: { getVoiceChannelId },
@@ -86,6 +88,7 @@ module.exports = (Plugin, Library) => {
             this.backFromAFKTimeoutID = undefined; // timeout set by onFocus
             // key used as a flag to detect if afk was set by the plugin or manually by user
             this.keyIdleSetByPlugin = "IdleSetByPlugin";
+            this.keyAlwaysOnline = "alwaysOnline"
 
             this.OnBlurBounded = this.onBlurFunction.bind(this);
             this.boundOnFocusBounded = this.onFocusFunction.bind(this);
@@ -126,6 +129,10 @@ module.exports = (Plugin, Library) => {
                 this._config.info.name,
                 this.keyIdleSetByPlugin
             );
+            var __alwaysOnline = BdApi.loadData(
+                this._config.info.name,
+                this.keyAlwaysOnline)
+
             var inVoiceChannelAndIdleSetByPlugin =
                 this.inVoiceChannel() && __afkSetByPlugin;
 
@@ -185,7 +192,17 @@ module.exports = (Plugin, Library) => {
                     this.currentStatus() === this.settings.afkStatus &&
                     __afkSetByPlugin === true;
 
+                var statusIsAFKAndAlwaysOnlineIsTrue = this.currentStatus() === this.settings.afkStatus && this.settings.alwaysOnline === true;
+
                 if (statusIsAFKAndWasSetByPlugin) {
+                    BdApi.showToast("Changing status back to online");
+                    this.updateStatus("online");
+                    BdApi.saveData(
+                        this._config.info.name,
+                        this.keyIdleSetByPlugin,
+                        false
+                    );
+                } else if (statusIsAFKAndAlwaysOnlineIsTrue) {
                     BdApi.showToast("Changing status back to online");
                     this.updateStatus("online");
                     BdApi.saveData(
@@ -264,3 +281,8 @@ module.exports = (Plugin, Library) => {
         }
     };
 };
+        return plugin(Plugin, Api);
+    })(global.ZeresPluginLibrary.buildPlugin(config));
+})();
+
+/*@end@*/
