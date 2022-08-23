7cec1d1b7d16d982e9884531f5c9a4d48c308bde
Haxurus <72351758+haxurus@users.noreply.github.com>
2022-08-23 19:14:37 +0200

    Say hello to version 0.3.2 [read desc.]
    Here are the new features now available:
    - decrease the min. time limit (from 5 minutes to 1 minute);
    - encrease the max. time limit (from 30 minutes to 12 hours);
    - choose if the plugin works only when you are "online" or with every status;
    - choose if come back to "online" or to a specific status.

--- release/AutoIdleOnAFK.plugin.js
+++ release/AutoIdleOnAFK.plugin.js
@@ -4,7 +4,7 @@
 Plugin only works when your status is 'online' and you are not in a voice channel. 
 
 For Bugs or Feature Requests open an issue on my Github
- * @version 0.3.1
+ * @version 0.3.2
  * @author RoguedBear
  * @authorLink https://github.com/RoguedBear
  * @website https://github.com/RoguedBear/BetterDiscordPlugin-AutoIdleOnAFK
@@ -33,7 +33,136 @@ For Bugs or Feature Requests open an issue on my Github
     WScript.Quit();
 
 @else@*/
-const config = {"info":{"name":"AutoIdleOnAFK","authors":[{"name":"RoguedBear","github_username":"RoguedBear","link":"https://github.com/RoguedBear"}],"version":"0.3.1","description":"Automatically updates your discord status to 'idle' when you haven't opened your discord client for more than 5 minutes.\nPlugin only works when your status is 'online' and you are not in a voice channel. \n\nFor Bugs or Feature Requests open an issue on my Github","github":"https://github.com/RoguedBear/BetterDiscordPlugin-AutoIdleOnAFK","github_raw":"https://github.com/RoguedBear/BetterDiscordPlugin-AutoIdleOnAFK/releases/latest/download/AutoIdleOnAFK.plugin.js"},"changelog":[{"type":"fixed","title":"Fixed the plugin :)","items":["Fixed what discord broke in recent update. (thanks `Zerthox` for helping me find the new module to use!)"]}],"main":"index.js","DEBUG":false,"DEBUG_ActuallyChangeStatus":false,"defaultConfig":[{"type":"radio","name":"Change Status To:","note":"the status selected will be switched to when AFK. default: Idle","id":"afkStatus","value":"idle","options":[{"name":"Idle","value":"idle"},{"name":"Invisible","value":"invisible"},{"name":"Do Not Disturb","value":"dnd"}]},{"type":"slider","name":"AFK Timeout (minutes)","note":"minutes to wait before changing your status to Idle/Invisible","id":"afkTimeout","value":5,"defaultValue":5,"min":5,"max":30,"units":"min","markers":[5,10,15,20,25,30]},{"type":"slider","name":"Back To Online Delay (Grace/Cooldown Period)","note":"seconds to wait before changing your status back to Online. if you close or unfocus discord window, status will not be changed","id":"backToOnlineDelay","defaultValue":10,"value":10,"min":5,"max":120,"units":"s","markers":[5,10,30,60,90,120]},{"type":"switch","name":"Always Revert To Online","note":"Come back online even if this plugin didn't change your status. Useful for multiple devices.","id":"alwaysOnline","value":false,"defaultValue":false},{"type":"switch","name":"Show toast messages?","note":"toggles the visibility of \"Changing status back to online\" toast message","id":"showToasts","defaultValue":true,"value":true}]};
+const config = {
+    "info":
+    {
+        "name": "AutoIdleOnAFK",
+        "authors":
+            [{
+                "name": "RoguedBear",
+                "github_username": "RoguedBear"
+            }],
+        "authorLink": "https://github.com/RoguedBear",
+        "version": "0.3.2",
+        "description": "Automatically updates your discord status to 'idle' when you haven't opened your discord client for more than 5 minutes.\nPlugin only works when your status is 'online' and you are not in a voice channel. \n\nFor Bugs or Feature Requests open an issue on my Github",
+        "github": "https://github.com/RoguedBear/BetterDiscordPlugin-AutoIdleOnAFK",
+        "github_raw": "https://github.com/RoguedBear/BetterDiscordPlugin-AutoIdleOnAFK/releases/latest/download/AutoIdleOnAFK.plugin.js"
+    },
+    "changelog":
+        [{
+            "type": "added",
+            "title": "What's New?",
+            "items":
+                ["Now you can choose if the plugin will change your status only from \"Online\" or from all the status.\n*[default: only from \"Online\"]*",
+                    "Updated time limits, now they are from ~~5 minutes~~ **1 minute** to ~~30 minutes~~ **12 hours**.\n*[default: 5 minutes]*",
+                    "Now you can choose the status for when you come back to your pc.\n*[default: \"Online\"]*"]
+        },
+        {
+            "title": "Fixed issues",
+            "type": "progress",
+            "items":
+                ["Now the plugin will correctly save the settings.",
+                    "Now your status will correctly set as \"Online\"."]
+            }
+            ,{
+                "title": "Removed:",
+                "type": "fixed",
+                "items":
+                    ["You can no longer set \"Invisible\" as status to set when you are back."]
+            },{
+                "type": "added",
+                "title": "Credits:",
+                "items":
+                    ["All these features have been added by Haxurus."]
+            }
+        ],
+    "main": "index.js",
+    "DEBUG": false,
+    "DEBUG_ActuallyChangeStatus": false,
+    "defaultConfig":
+        [{
+            "type": "radio",
+            "name": "Change Status To:",
+            "note": "the status selected will be switched to when AFK. default: Idle",
+            "id": "afkStatus",
+            "value": "idle",
+            "options":
+                [{ "name": "Idle", "value": "idle" },
+                { "name": "Invisible", "value": "invisible" },
+                { "name": "Do Not Disturb", "value": "dnd" }]
+        },
+        {
+            "type": "slider",
+            "name": "AFK Timeout (minutes)", "note": "minutes to wait before changing your status to Idle/Invisible",
+            "id": "afkTimeoutMin",
+            "value": 1,
+            "defaultValue": 5,
+            "min": 1,
+            "max": 30,
+            "units": "min",
+            "markers": [1, 5, 10, 15, 20, 25, 30]
+        },
+        {
+            "type": "slider",
+            "name": "AFK Timeout (minutes)", "note": "hours to wait before changing your status to Idle/Invisible",
+            "id": "afkTimeoutH",
+            "value": 0,
+            "defaultValue": 0,
+            "min": 0,
+            "max": 12,
+            "units": "h",
+            "markers": [0, 1, 2, 6, 12]
+        },
+        {
+            "type": "slider",
+            "name": "Back To Online Delay (Grace/Cooldown Period)",
+            "note": "seconds to wait before changing your status back to Online. if you close or unfocus discord window, status will not be changed",
+            "id": "backToOnlineDelay",
+            "defaultValue": 10,
+            "value": 10,
+            "min": 5,
+            "max": 120,
+            "units": "s",
+            "markers": [5, 10, 30, 60, 90, 120]
+        },
+        {
+            "type": "switch",
+            "name": "Always Revert To Online",
+            "note": "Come back online even if this plugin didn't change your status. Useful for multiple devices.",
+            "id": "alwaysOnline",
+            "value": false,
+            "defaultValue": false
+        },
+        {
+            "type": "switch",
+            "name": "Show toast messages?",
+            "note": "toggles the visibility of \"Changing status back to online\" toast message",
+            "id": "showToasts",
+            "defaultValue": true,
+            "value": true
+        },
+        {
+            "type": "switch",
+            "name": "Change status only from \"Online\"?",
+            "note": "change your status only if you are actually \"Online\"",
+            "id": "onlyOnline",
+            "defaultValue": true,
+            "value": true
+        },
+        {
+            "type": "radio",
+            "name": "When you return, set status to:",
+            "note": "the status selected will be switched to when you will be back. default: Online",
+            "id": "goBackStatus",
+            "value": "online",
+            "options":
+                [{ "name": "Online", "value": "online" },
+                { "name": "Idle", "value": "idle" },
+                { "name": "Do Not Disturb", "value": "dnd" }]
+        }
+        ]
+};
+
 class Dummy {
     constructor() {this._config = config;}
     start() {}
@@ -54,7 +183,7 @@ if (!global.ZeresPluginLibrary) {
 }
  
 module.exports = !global.ZeresPluginLibrary ? Dummy : (([Plugin, Api]) => {
-     const plugin = (Plugin, Library) => {
+    const plugin = (Plugin, Library) => {
     const { Logger, DiscordModules } = Library;
     const {
         SelectedChannelStore: { getVoiceChannelId },
@@ -97,9 +226,7 @@ module.exports = !global.ZeresPluginLibrary ? Dummy : (([Plugin, Api]) => {
                 log_debug(this);
                 log_debug("Current status: " + this.currentStatus());
                 log_debug("In Voice Channel: " + this.inVoiceChannel());
-                log_debug(
-                    "onlineStatusAndNotInVC: " + this.onlineStatusAndNotInVC()
-                );
+                log_debug("onlineStatusAndNotInVC: " + this.onlineStatusAndNotInVC());
             }
             window.addEventListener("blur", this.OnBlurBounded);
             window.addEventListener("focus", this.boundOnFocusBounded);
@@ -134,7 +261,7 @@ module.exports = !global.ZeresPluginLibrary ? Dummy : (([Plugin, Api]) => {
 
             if (this.onlineStatusAndNotInVC()) {
                 var _timeout_ms =
-                    this.settings.afkTimeout * (DEBUG ? 2 : 60) * 1000;
+                    this.settings.afkTimeoutMin * (DEBUG ? 2 : 60) * 1000 + this.settings.afkTimeoutH * (DEBUG ? 2 : 60) * 1000 * 60;
                 log_debug("setting timeout of " + _timeout_ms + "ms");
                 this.afkTimeoutID = setTimeout(() => {
                     if (this.onlineStatusAndNotInVC()) {
@@ -193,16 +320,16 @@ module.exports = !global.ZeresPluginLibrary ? Dummy : (([Plugin, Api]) => {
                     this.settings.alwaysOnline === true;
 
                 if (statusIsAFKAndWasSetByPlugin) {
-                    this.showToast("Changing status back to online");
-                    this.updateStatus("online");
+                    this.showToast("Changing status back to " + this.settings.goBackStatus);
+                    this.updateStatus(this.settings.goBackStatus);
                     BdApi.saveData(
                         this._config.info.name,
                         this.keyIdleSetByPlugin,
                         false
                     );
                 } else if (statusIsAFKAndAlwaysOnlineIsTrue) {
-                    BdApi.showToast("Changing status back to online");
-                    this.updateStatus("online");
+                    BdApi.showToast("Changing status back to " + this.settings.goBackStatus);
+                    this.updateStatus(this.settings.goBackStatus);
                     BdApi.saveData(
                         this._config.info.name,
                         this.keyIdleSetByPlugin,
@@ -254,8 +381,13 @@ module.exports = !global.ZeresPluginLibrary ? Dummy : (([Plugin, Api]) => {
          * @returns
          */
         onlineStatusAndNotInVC() {
+            if (this.settings.onlyOnline) {
+                return (
+                    this.currentStatus() === "online" &&
+                    this.inVoiceChannel() === false
+                );
+            }
             return (
-                this.currentStatus() === "online" &&
                 this.inVoiceChannel() === false
             );
         }
